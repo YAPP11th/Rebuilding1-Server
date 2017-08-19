af8f2a771eef302aa7cec9ac9aad3cad1e97131a
ridickle <ridickle7@gmail.com>
2017-08-19 21:38:30 +0900

    first Commit

--- routes/users.js
+++ routes/users.js
@@ -7,28 +7,26 @@ router.get('/', function (req, res, next) {
 });
 
 var FCM = require('fcm').FCM;
-var apiKey = 'AAAAmJYRFQQ:APA91bGTzFVV3crC-bgUU8Lmc8r0ZB3cKEKHQKcWbIogfmtO7jky3f1IEv_euepUv62tYJvdBCtOKs7rtPqweHZywfZYylj9Dl-DxJy1oIBwsAtwrPIzqo4GLMpxbUndEenxWVhXd-lf';
+var apiKey = require('../auth/firebase_con').apiKey();
 var fcm = new FCM(apiKey);
 
-router.post('/pushExample', function(req,res,next){
+router.post('/pushExample', function (req, res, next) {
   var message = {
-    registration_id: 'cPDDMY3rjFo:APA91bHYuSvuK_MT00fiVkkvUXWtaAfr8vcO7GSWwBqlyIzVYC89SkltI_CEGtmNTQciaFLQF13dbQx0GiLM1owq7cASe5OXuczQvH5zVxTB0owch_RdRKVpNoaNPiQs1tsSihg4vy0F', // required 
+    registration_id: require('../auth/firebase_con').registration_id(),   // required 
     collapse_key: 'Collapse key',
     data1: req.body.data1,
     data2: req.body.data2
   };
-  
+
   fcm.send(message, function (err, messageId) {
     if (err) {
       console.log("Something has gone wrong!");
     }
-    else { 
-      console.log("Sent with message ID: ", messageId); 
+    else {
+      console.log("Sent with message ID: ", messageId);
     }
   });
-
-  res.json({data1 : req.body.data1, data2 : req.body.data2});
+  res.json({ data1: req.body.data1, data2: req.body.data2 });
 });
-
 
 module.exports = router;
